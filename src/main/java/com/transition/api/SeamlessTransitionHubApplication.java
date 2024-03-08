5bd3b628307c4b35ef48a9269ca196b45454128b
ANKIT CHOUDHARY <ankitkrchy@gmail.com>
2024-03-08 18:42:15 +0530

    Initial commit

---
+++ src/main/java/com/transition/api/SeamlessTransitionHubApplication.java
@@ -0,0 +1,13 @@
+package com.transition.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeamlessTransitionHubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeamlessTransitionHubApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transition/api/SeamlessTransitionHubApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transition.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeamlessTransitionHubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
